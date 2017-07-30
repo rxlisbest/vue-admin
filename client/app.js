712b465481af6613aa8e4b15771ab042c77ef5b0
roy <rxlisbest@163.com>
2017-07-30 21:25:10 +0800

    project init

---
+++ client/app.js
@@ -0,0 +1,80 @@
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueAuth from '@websanova/vue-auth'
+import NProgress from 'vue-nprogress'
+import { sync } from 'vuex-router-sync'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import * as filters from './filters'
+import { TOGGLE_SIDEBAR } from 'vuex-store/mutation-types'
+
+Vue.router = router
+
+Vue.use(VueAxios, axios)
+Vue.use(VueAuth, {
+  auth: {
+    request: function (req, token) {
+      this.options.http._setHeaders.call(this, req, {Authorization: 'Bearer ' + token})
+    },
+    response: function (res) {
+      // Get Token from response body
+      return res.data.access_token
+    }
+  },
+  http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
+  router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
+  loginData: { url: 'http://localhost:8000/api/v1/auth', fetchUser: false },
+  refreshData: { enabled: false }
+})
+// POST传参序列化
+axios.interceptors.request.use((config) => {
+  // console.log(config);
+  // config.params.access_token = "-gF0w_e6ykO1WJbOeRS_AKeBzpiqqthlth9Vyo2s";
+  return config;
+},(error) =>{
+  alert("错误的传参");
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use((res) =>{
+  if(res.status != '200'){
+    console.log(res)
+    return res;
+  }
+  return res;
+}, (error) => {
+  // alert("网络异常");
+  // return res;
+});
+
+Vue.use(NProgress)
+
+// Enable devtools
+Vue.config.devtools = true
+
+sync(store, router)
+
+const nprogress = new NProgress({ parent: '.nprogress-container' })
+
+const { state } = store
+
+router.beforeEach((route, redirect, next) => {
+  if (state.app.device.isMobile && state.app.sidebar.opened) {
+    store.commit(TOGGLE_SIDEBAR, false)
+  }
+  next()
+})
+
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
+const app = new Vue({
+  router,
+  store,
+  nprogress,
+  ...App
+})
+export { app, router, store }
---
+++ client/views/auth/Login.vue
@@ -0,0 +1,111 @@
+<template>
+<div class="content has-text-centered">
+  <h1 class="is-title is-bold">Login</h1>
+
+  <div class="columns is-vcentered">
+    <div class="column is-6 is-offset-3">
+      <div class="box">
+        <div v-show="error" style="color:red; word-wrap:break-word;">{{ error }}</div>
+        <form v-on:submit.prevent="login">
+          <label class="label">Email</label>
+          <p class="control">
+            <input v-model="data.body.username" class="input" type="text" placeholder="email@example.org">
+          </p>
+          <label class="label">Password</label>
+          <p class="control">
+            <input v-model="data.body.password" class="input" type="password" placeholder="password">
+          </p>
+
+          <p class="control">
+            <label class="checkbox">
+              <input type="checkbox" v-model="data.rememberMe">
+              Remember me
+            </label>
+          </p>
+
+          <hr>
+          <p class="control">
+            <button type="submit" class="button is-primary">Login</button>
+            <button class="button is-default">Cancel</button>
+          </p>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+      data: {
+        body: {
+          grant_type: 'password',
+          client_id: 'blog',
+          scop: 'test',
+          username: null,
+          password: null
+        },
+        rememberMe: false
+      },
+      error: null
+    }
+  },
+  mounted () {
+    if (this.$auth.redirect()) {
+      console.log('Redirect from: ' + this.$auth.redirect().from.name)
+    }
+    // Can set query parameter here for auth redirect or just do it silently in login redirect.
+  },
+  methods: {
+    login () {
+      var redirect = this.$auth.redirect()
+      console.log(redirect)
+      this.$auth.login({
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        params: this.data.body,
+        rememberMe: this.data.rememberMe,
+        redirect: {name: redirect ? redirect.from.name : 'Home'},
+        success (res) {
+          this.$auth.token('test')
+          console.log('Auth Success')
+          console.log('Token: ' + this.$auth.token())
+          console.log(res)
+        },
+        error (err) {
+          console.log(this.data.body);
+          if (err.response) {
+            // The request was made, but the server responded with a status code
+            // that falls out of the range of 2xx
+            // console.log(err.response.status)
+            // console.log(err.response.data)
+            // console.log(err.response.headers)
+            this.error = err.response.data
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', err.message)
+          }
+          console.log(err.config)
+        }
+      })
+    }
+  }
+  // filters: {
+  //   json: function (value) {
+  //     console.log(value)
+  //     return value
+  //   }
+  // }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.is-title {
+    text-transform: capitalize;
+}
+</style>
